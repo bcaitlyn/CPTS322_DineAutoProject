10e2441b6e7d8aad3da0a88c9713af988ce12b95
emilywest1 <146793504+emilywest1@users.noreply.github.com>
2025-03-13 20:29:39 -0700

    customer registration

--- obj/Debug/net9.0/starter project.AssemblyInfo.cs
+++ obj/Debug/net9.0/starter project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("starter project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0cc3a16e69536336a71914146d1fc47981b558f")]
 [assembly: System.Reflection.AssemblyProductAttribute("starter project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("starter project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

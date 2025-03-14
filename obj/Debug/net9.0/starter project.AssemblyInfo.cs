04154097a5af19de4a613c57e4feedb4896a57e9
Kaden Metzger <kaden.metzger@gmail.com>
2025-03-14 03:57:13 -0700

    added a sln file so it will compile lol

--- obj/Debug/net9.0/starter project.AssemblyInfo.cs
+++ obj/Debug/net9.0/starter project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("starter project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0435aed4ef6c8ce894303db312da8440b3a1468f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e0639d1a806617aa21d7d14e98476a91abb25cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("starter project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("starter project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

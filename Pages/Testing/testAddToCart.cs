91614f98156643c78783b3bdcd95fcc3b8a1c304
Kaden Metzger <kaden.metzger@gmail.com>
2025-04-24 13:09:08 -0700

    Created tests for clear cart, add to cart, and login features

---
+++ Pages/Testing/testAddToCart.cs
@@ -0,0 +1,92 @@
+﻿//file created on 4/24/25
+
+using DineAuto.Pages.Cart;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+namespace DineAuto.Testing
+{
+    public partial class Tests
+    {
+        /// <summary>
+        /// Method taken from CartMethods but modified for testing files.
+        /// </summary>
+        public static Dictionary<string, CartObj> LoadUsersCart()
+        {
+            string filePath = "Pages/Testing/TestingTables/testCarts.json";
+            if (System.IO.File.Exists(filePath))
+            {
+                string json = System.IO.File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<Dictionary<string, CartObj>>(json) ?? new Dictionary<string, CartObj>();
+            }
+            return new Dictionary<string, CartObj>();
+        }
+
+        /// <summary>
+        /// Method taken from CartMethods but modified for testing files.
+        /// </summary>
+        public static void SaveUsersCart(Dictionary<string, CartObj> usersCart)
+        {
+            string filePath = "Pages/Testing/TestingTables/testCarts.json";
+            string json = JsonConvert.SerializeObject(usersCart, Formatting.Indented);
+            System.IO.File.WriteAllText(filePath, json);
+        }
+
+        /// <summary>
+        /// Method taken from CartMethods but modified for testing files.
+        /// </summary>
+        public void AddUserCart(Dictionary<string, CartObj> usersCarts, string username)
+        {
+            usersCarts = LoadUsersCart();
+            if (!usersCarts.ContainsKey(username))
+            {
+                usersCarts[username] = new CartObj();
+            }
+        }
+
+        public static void TestAddItemToCart()
+        {
+            Item burger = new Item("Cheese Burger", 12, "Coug Eats", new Guid());
+            Dictionary<string, CartObj> usersCarts = LoadUsersCart();
+            CartObj userCart = usersCarts["TestName"];
+            userCart.AddItem(burger);
+            SaveUsersCart(usersCarts);
+
+            string jsonFile = File.ReadAllText("Pages/Testing/TestingTables/testCarts.json");
+
+            if(jsonFile.Contains("Cheese Burger") && jsonFile.Contains("Coug Eats"))
+            {
+                Console.WriteLine("Add Item To Cart Test PASSED");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Add Item To Cart Test FAILED");
+                return;
+            }
+        }
+
+        public static void TestClearCart()
+        {
+            Dictionary<string, CartObj> usersCarts = LoadUsersCart();
+            CartObj userCart = usersCarts["TestName"];
+            userCart.items.Clear();
+            SaveUsersCart(usersCarts);
+
+            string jsonFile = File.ReadAllText("Pages/Testing/TestingTables/testCarts.json");
+
+            if (!jsonFile.Contains("Cheese Burger") && !jsonFile.Contains("Coug Eats"))
+            {
+                Console.WriteLine("Clear Cart Test PASSED");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Clear Cart Test FAILED");
+                return;
+            }
+
+        }
+
+    }
+}
---
+++ Pages/Testing/testLogin.cs
@@ -0,0 +1,130 @@
+﻿namespace DineAuto.Testing
+{
+    using DineAuto.Pages.LoginMethods;
+    using Newtonsoft.Json;
+
+    public partial class Tests
+    {
+        /// <summary>
+        /// Helper method for loading users, same as LoginMethods except the filePath has been swapped to a test json file.
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<string, string> LoadUsers(string filePath)
+        {
+            if (System.IO.File.Exists(filePath))
+            {
+                string json = System.IO.File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<Dictionary<string, string>>(json) ?? new Dictionary<string, string>();
+            }
+            return new Dictionary<string, string>();
+
+        }
+
+        /// <summary>
+        /// Slightly modified VerifyLogin Method, will be used to check if login is valid.
+        /// </summary>
+        /// <param name="users"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static bool VerifyLogin(Dictionary<string, string> users, string username, string password)
+        {
+            // If our users dictionary doesnt contain the username return false.
+            if (!(users.ContainsKey(username)))
+            {
+
+                return false;
+
+            }
+            else
+            {
+                // Since the username exists, check if the password matches our database.
+                if (BCrypt.Net.BCrypt.Verify(password, users[username]))
+                {
+                    return true;
+                }
+                else
+                {
+
+                    return false;
+
+                }
+            }
+        }
+
+        public static void TestCustomerLogin()
+        {
+            string filePath = "Pages/Testing/TestingTables/testCustomers.json";
+            Dictionary<string, string> users = LoadUsers(filePath);
+            string username = "TestName";
+            string password = "TestPW";
+            if(VerifyLogin(users, username, password))
+            {
+                Console.WriteLine("Customer Login Test PASSED!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Customer Login Test FAILED!");
+                return;
+            }
+        }
+
+        public static void TestEmployeeLogin()
+        {
+            string filePath = "Pages/Testing/TestingTables/testEmployees.json";
+            Dictionary<string, string> users = LoadUsers(filePath);
+            string username = "EmployeeAccountTest";
+            string password = "12345";
+            if (VerifyLogin(users, username, password))
+            {
+                Console.WriteLine("Employee Login Test PASSED!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Employee Login Test FAILED!");
+                return;
+            }
+        }
+
+        public static void TestOwnerLogin()
+        {
+            string filePath = "Pages/Testing/TestingTables/testOwners.json";
+            Dictionary<string, string> users = LoadUsers(filePath);
+            string username = "OwnerAccountTest";
+            string password = "12345";
+            if (VerifyLogin(users, username, password))
+            {
+                Console.WriteLine("Owner Login Test PASSED!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Owner Login Test FAILED!");
+                return;
+            }
+        }
+
+        public static void TestAdminLogin()
+        {
+            string filePath = "Pages/Testing/TestingTables/testAdmins.json";
+            Dictionary<string, string> users = LoadUsers(filePath);
+            string username = "TestAdmin";
+            string password = "12345";
+            if (VerifyLogin(users, username, password))
+            {
+                Console.WriteLine("Admin Login Test PASSED!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Admin Login Test FAILED!");
+                return;
+            }
+        }
+
+
+
+    }
+}
